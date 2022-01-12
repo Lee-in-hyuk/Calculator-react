5085499100e1396525bef3af496d3a8fae9d2ae1
hyuk <dlsgur0906@gmail.com>
2022-01-12 17:43:07 +0900

    input에 버튼값 들어가는거까지 완성, join메서드 적용해보자!

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 import Display from './components/display';
 import Keypad from './components/keypad';
 import Button from './components/button';
-import { useState,useRef, useCallback } from 'react';
+import { useState, useCallback } from 'react';
+
+
+//https://ichi.pro/ko/react-jsleul-sayonghayeo-gandanhan-gyesangi-mandeulgi-259193783442851
+// 이거 보면서 작성완성 시켜보기
 
 import './App.css';
 //eval함수써서 해보기 (사칙연산을 해주는 함수)
 function App() {
   const [ input, setInput ] = useState("");
-  // console.log(input);
+  // let a = input.join("");
+  console.log(input);
+
+  //onChange 없어도 작동은 잘 되는데, value를 onChange없이 썼다고 console에 경고떠서 작성함.
   const onChange = useCallback((e)=>{
     const btnValue = e.target.value;
     setInput(btnValue);
-  },[input])
+  },[])
 
   function onClick(e){
     const btnValue = e.target.value;
+    setInput([
+      ...input,
+      btnValue
+    ]);
+  }
+
+  // C버튼 클릭 시 "" 값이 들어가는 함수
+  function reset(e){
+    const btnValue = e.target.value;
     setInput(btnValue);
   }
+
+  // function add
+
   return (
     <div className="App">
       <Display input={input} onChange={onChange}/>
         <Keypad>
-            <Button label="C" value="clear" />
+            <Button label="C" value="" onClick={reset}/>
             <Button label="7" value="7" onClick={onClick}/>
             <Button label="4" value="4" onClick={onClick}/>
             <Button label="1" value="1" onClick={onClick}/>
